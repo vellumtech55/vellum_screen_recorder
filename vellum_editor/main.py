ec9106e960254398b0cb856384a1b13c4392ff8f
vellum_tech <dragonwarboy55@gmail.com>
2026-07-07 23:16:06 -0400

    Delete vellum_editor directory

--- vellum_editor/main.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-from core.app import VellumTool
-
-if __name__ == "__main__":
-    app = VellumTool()
-    app.mainloop()
